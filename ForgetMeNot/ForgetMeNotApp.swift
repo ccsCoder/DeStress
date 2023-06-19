b3eca6d014069a6913255dffaea15bc0f14d68e2
Nishant Gupta <nishangu@google.com>
2023-06-19 18:44:16 +0530

    Initial Commit

---
+++ ForgetMeNot/ForgetMeNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgetMeNotApp.swift
+//  ForgetMeNot
+//
+//  Created by Nishant Gupta on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ForgetMeNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
